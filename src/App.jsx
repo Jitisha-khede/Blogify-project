86bb8d31c91421b118866b5201924b20efeb4b8a
Jitisha Khede <khedejitisha2004@gmail.com>
2025-01-13 01:52:06 +0530

    created getting started page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import React from "react";
+"use client";
+import { GettingStarted } from './components/ui/getting-started-page';
+
+function App() {
+
+  return(
+    <div>
+      <GettingStarted/>
+    </div>
+  )
+}
+
+export default App
