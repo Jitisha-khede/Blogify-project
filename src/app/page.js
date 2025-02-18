4d6d8029e4ebcf468cc04e65055c11ed54e458c7
Jitisha Khede <khedejitisha2004@gmail.com>
2025-02-18 22:57:58 +0530

    fixed minor bugs

--- src/app/page.js
+++ src/app/page.js
@@ -10,11 +10,7 @@ import React from 'react';
 import { ThemeProvider } from '@/components/ui/ThemeContext';
 import { NavBar } from '../components/navbar';
 import { GettingStarted } from '../components/getting-started-page';
-<<<<<<< HEAD
-import Blogs from '../components/home';
-=======
 import Catelog from '../components/home';
->>>>>>> 2beaa133022ef0f2c7f0944e9e03c028f1d1e957
 import Blog from '@/components/blog';
 import Create from '@/components/create';
 
@@ -27,13 +23,8 @@ function Layout() {
 			{!hideNavbar && <NavBar />}
 			<Routes>
 				<Route path='/' element={<GettingStarted />} />
-<<<<<<< HEAD
-				<Route path='/blogs' element={<Blogs />} />
-				<Route path='/blog' element={<Blog />} />
-=======
 				<Route path='/create' element={<Create />} />
 				<Route path='/blogs' element={<Catelog />} />
->>>>>>> 2beaa133022ef0f2c7f0944e9e03c028f1d1e957
 			</Routes>
 		</>
 	);
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,11 +5,7 @@ import { HoverEffect } from './ui/card-container';
 import { fetchBlogs } from '@/utils/api'; // Assuming you've created the API fetch function
 import Link from 'next/link';
 
-<<<<<<< HEAD
-const Blogs = () => {
-=======
 const Catelog = () => {
->>>>>>> 2beaa133022ef0f2c7f0944e9e03c028f1d1e957
 	const [blogs, setBlogs] = useState([]);
 	const [loading, setLoading] = useState(true); // Loading state
 	const [error, setError] = useState(null); // Error state
@@ -59,8 +55,4 @@ const Catelog = () => {
 	);
 };
 
-<<<<<<< HEAD
-export default Blogs;
-=======
 export default Catelog;
->>>>>>> 2beaa133022ef0f2c7f0944e9e03c028f1d1e957
