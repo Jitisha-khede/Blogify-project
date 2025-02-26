e90c5322141737bb8fb758bf1b4cca681ff0ceea
Jitisha Khede <khedejitisha2004@gmail.com>
2025-02-26 17:51:08 +0530

    fixed small bug

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 'use client';
 
+import React, { useState } from 'react';
 import {
 	BrowserRouter as Router,
 	Routes,
 	Route,
 	useLocation,
 } from 'react-router-dom';
-import React from 'react';
+// import React, { useState } from 'react';
 import { ThemeProvider } from '@/components/ui/ThemeContext';
 import { NavBar } from '../components/navbar';
 import { GettingStarted } from '../components/getting-started-page';
@@ -17,15 +18,19 @@ import Create from '@/components/createBlog';
 function Layout() {
 	const location = useLocation();
 	const hideNavbar = location.pathname === '/';
+	const [searchResults, setSearchResults] = useState(null);
 
 	return (
 		<>
-			{!hideNavbar && <NavBar />}
+			{!hideNavbar && <NavBar setSearchResults={setSearchResults}/>}
 			<Routes>
 				<Route path='/' element={<GettingStarted />} />
 				{/* <Route path='/blogs' element={<Home />} /> */}
 				<Route path='/create' element={<Create />} />
-				<Route path='/blogs' element={<Catelog />} />
+				<Route
+					path='/blogs'
+					element={<Catelog blogs={searchResults || undefined} setSearchResults={setSearchResults} />}
+				/>
 				<Route path='/blog/:id' element={<Blog />} />
 			</Routes>
 		</>
